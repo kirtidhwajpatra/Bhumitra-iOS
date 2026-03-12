3d24497732f29b8360fc4aaa88eef9b01b901fd7
kirtidhwaj <kirtidhwajpatra@gmail.com>
2026-03-12 09:55:17 +0530

    Initial Commit

---
+++ MyBhoomi/MyBhoomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBhoomiApp.swift
+//  MyBhoomi
+//
+//  Created by Mr SwiftUI on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBhoomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
